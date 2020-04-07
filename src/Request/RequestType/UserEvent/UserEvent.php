ddaac720826b73acc9e6b033b885ff86c467182c
Ralf Eggert <ralf@travello.com>
2020-04-07 14:13:52 +0200

    Added user event

---
+++ src/Request/RequestType/UserEvent/UserEvent.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+namespace Phlexa\Request\RequestType\UserEvent;
+
+use function count;
+
+class UserEvent implements UserEventInterface
+{
+    /** @var array */
+    private $arguments;
+
+    /** @var string */
+    private $token;
+
+    /** @var array */
+    private $components;
+
+    /**
+     * UserEvent constructor.
+     *
+     * @param array  $arguments
+     * @param string $token
+     * @param array  $components
+     */
+    public function __construct(array $arguments, string $token, array $components)
+    {
+        $this->arguments  = $arguments;
+        $this->token      = $token;
+        $this->components = $components;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @return array
+     */
+    public function getComponents():array
+    {
+        return $this->components;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return string
+     */
+    public function getComponentValue(string $key): string
+    {
+        return $this->components[$key];
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function hasValidComponentValue(string $key):bool
+    {
+        return isset($this->components[$key]);
+    }
+}
---
+++ src/Request/RequestType/UserEventRequestType.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Build voice applications for Amazon Alexa with phlexa and PHP
+ *
+ * @author     Ralf Eggert <ralf@travello.audio>
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link       https://github.com/phoice/phlexa
+ * @link       https://www.phoice.tech/
+ * @link       https://www.travello.audio/
+ */
+
+declare(strict_types=1);
+
+namespace Phlexa\Request\RequestType;
+
+use Phlexa\Request\RequestType\UserEvent\UserEventInterface;
+
+/**
+ * Class UserEventRequestType
+ *
+ * @package Phlexa\Request\RequestType
+ */
+class UserEventRequestType extends AbstractRequestType
+{
+    public const NAME = 'Alexa.Presentation.APL.UserEvent';
+
+    /** @var string */
+    private $type = 'Alexa.Presentation.APL.UserEvent';
+
+    /** @var UserEventInterface */
+    private $userEvent;
+
+    /**
+     * LaunchRequestType constructor.
+     *
+     * @param string $requestId
+     * @param string $timestamp
+     * @param string $locale
+     */
+    public function __construct(
+        string $requestId,
+        string $timestamp,
+        string $locale,
+        UserEventInterface $userEvent
+    ) {
+        $this->requestId = $requestId;
+        $this->timestamp = $timestamp;
+        $this->locale    = $locale;
+        $this->userEvent = $userEvent;
+    }
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return UserEventInterface
+     */
+    public function getUserEvent(): UserEventInterface
+    {
+        return $this->userEvent;
+    }
+
+
+
+    
+}
